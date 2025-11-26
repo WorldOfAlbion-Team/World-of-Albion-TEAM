b9860aca22f0c41f7057f04aebafbf674e3c8cda
WorldOfAlbion-Team <mauriciodert@gmail.com>
2025-11-26 13:43:11 -0500

    Update whitelist.js

--- src/config/whitelist.js
+++ src/config/whitelist.js
@@ -1,5 +1,6 @@
 // src/config/whitelist.js
 export const allowedGuilds = [
+  "1381130136677777518", // Kings
   "1417511080091062347", // Servidor de desarrollo
   "1439061223990886513"  // Servidor de producción
 ];
